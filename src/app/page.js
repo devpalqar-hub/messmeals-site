5fc486e75d2330daee929067230916d995ee1b9a
SaiSandeep571 <saisandeep@palqar.cloud>
2025-10-29 22:50:27 +0530

    Contact and Plan section

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,16 @@ import Image from "next/image";
 import Header from "./Sections/Header/header";
 import Hero from "./Sections/Hero/hero";
 import Footer from "./Sections/Footer/Footer";
+import Contact from "./Sections/Contact/Contact";
+import Plans from "./Sections/Plans/Plans";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Hero />
+      <Plans/>
+      <Contact/>
       <Footer/>
     </div>
   );
